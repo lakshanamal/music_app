859cc1ab1369e1f55f86721edd3d62f6fe9e0483
lakshanamal <lakshanamal100@gmail.com>
2021-06-02 14:58:27 +0530

    apply body

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 
+
+// main method in flutter
 void main() {
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    
       home: MusicApp(),
     );
   }
 }
 
 class MusicApp extends StatefulWidget {
-
   @override
   _MusicAppState createState() => _MusicAppState();
 }
@@ -30,6 +31,12 @@ class _MusicAppState extends State<MusicApp> {
         backgroundColor: Colors.blue,
         title: Text("My Music Player"),
         elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Expanded(child: ListView(),),
+          Container()
+        ],
       ),
       
     );
